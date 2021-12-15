1d9e55ac6e3d6700ea5c69cbcbc4387450d91fd0
suhendro-dev <halim.suhendro21@gmail.com>
2021-12-15 21:35:59 +0700

    project capstone

---
+++ app/src/main/java/com/example/aplikasikonsultasidokter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasikonsultasidokter.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplikasikonsultasidokter.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
